6c3465213e10ac2869e35ed9d1c0812d5916efd2
Funeralsawa <ox7zcdbb@gmail.com>
2025-08-18 14:13:57 +0800

    add info-drawer-content

--- src/components/infoDrawer.jsx
+++ src/components/infoDrawer.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import HistoryBlog from './historyBlog';
+import BlogData from './blogData';
+import headImg from '../assets/head.jpg'
 
 class InfoDrawer extends Component {
     state = {  } 
     render() { 
         return (
             <React.Fragment>
-                
+                <div className='info-drawer-content'>
+                    <div className="info-drawer-content-bloginfo">
+                        <img src={headImg} alt="headImg" draggable="false" />
+                        <BlogData />
+                    </div>
+                    <div className="content-aside-newest-essay info-drawer-content-essay">
+                        <HistoryBlog />
+                    </div>
+                </div>
             </React.Fragment>
         );
     }
