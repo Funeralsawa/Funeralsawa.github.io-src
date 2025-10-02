d94f4e0505e4cb12bf8eb13d3a25b8aef9cf2c3f
Funeralsawa <ox7zcdbb@gmail.com>
2025-10-02 22:36:47 +0800

    mend scroll bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/index.css'
 import "./css/typeWriter.css"
@@ -8,12 +8,23 @@ import SiteWrapper from './components/site.jsx'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
 import "bootstrap-icons/font/bootstrap-icons.min.css"
-import { HashRouter } from 'react-router-dom'
+import { HashRouter, useLocation } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+
+  return null;
+}
+
 createRoot(document.getElementById('root')).render(
   <HashRouter>
+    <ScrollToTop />
     <Provider store={store}>
       <div className="container">
         <SiteWrapper />
